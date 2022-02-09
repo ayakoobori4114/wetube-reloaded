11ce6650dd630a5fe64298a564a18e8f5043801c
ayakoobori4114 <ayakoobori4114@gmail.com>
2022-02-09 20:48:35 +0900

    middleware study
    study

--- src/server.js
+++ src/server.js
@@ -4,34 +4,19 @@ const PORT = 4000;
 
 const app = express(); //express application설계를 위한 규칙 / express function
 //express와 관랸된 코드는 express application을 만들고 나서 진행해야됨. 다음부터 코드를 작성 (샌드위치같이)
-const gossipMiddleware = (req, res, next) => {
-    console.log("I'm middle ware");
-    next();
-}
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected"){
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    next();
-}
-
-const handleProtected = (req, res) => {
-    return res.send("welcome to the rounge!");
-}
-
-const handleHome = (req, res) => {
-    return res.send("<h1>i love you</h1>");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next(); //함수가 next를 호출하면 middleware임
 };
 
-app.use(gossipMiddleware);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", gossipMiddleware, handleProtected);
+const handleHome = (req, res) => {
+  return res.send("<h1>i love you</h1>"); //send는 middleware가 아님 *연결이 중단되기 때문에
+};
 
-const handleListening = () => 
-    console.log(`Server listenting on port http://localhost:${PORT} 😼`);
+app.get("/", logger, handleHome);
+
+const handleListening = () =>
+  console.log(`Server listenting on port http://localhost:${PORT} 😼`);
 //서버가 만들어짐 4000포트에
-app.listen(PORT, handleListening);//port, callback; 서버가 항상 기다리게 만듦 
+app.listen(PORT, handleListening); //port, callback; 서버가 항상 기다리게 만듦
 //내 서버에 가는방법 -> localhost:portnum 4000
